a4473a3c8ffe6a8bec4efd877a71942a3810c6cf
djx <jxce_d@163.com>
2017-03-09 16:53:55 +0800

    ok

--- WebApplication5/ashx/ImgVcode.ashx.cs
+++ WebApplication5/ashx/ImgVcode.ashx.cs
@@ -16,18 +16,18 @@ namespace WebApplication5.ashx
         public void ProcessRequest(HttpContext context)
         {
             context.Response.ContentType = "image/jpeg";
-            using (Image img = new Bitmap(50, 21))
+            using (Image img = new Bitmap(55, 25))
             {
                 using (Graphics gra = Graphics.FromImage(img))
                 {
                     gra.Clear(Color.DarkSeaGreen);
-                    StringBuilder s = Getstring(4);
+                    string s = Getstring(4);
                     context.Session["vcode"] = s.ToString().ToLower();
-                    gra.DrawString(s.ToString(), new Font("宋体",14, FontStyle.Strikeout),new SolidBrush(Color.DarkRed),new PointF(3,5));
+                    gra.DrawString(s, new Font("宋体",18, FontStyle.Strikeout),new SolidBrush(Color.DarkRed),new PointF(1,1));
                     gra.DrawLines(Pens.AliceBlue, new PointF[] {
-                        new PointF(0,6),
-                        new PointF(50,5),
-                        new PointF(0,18),
+                        new PointF(0,8),
+                        new PointF(50,10),
+                        new PointF(0,20),
                         new PointF(50,16)
                     });
                     
@@ -36,7 +36,7 @@ namespace WebApplication5.ashx
             }
             
         }
-        private StringBuilder Getstring(int count)
+        private string Getstring(int count)
         {
             Random r = new Random();
             string[] s = {"a","b","c","d","e","h","g","2","3","4","5", "A", "B", "C", "D", "E", "F", "G", "6", "7", "9", "y","k" };
@@ -45,7 +45,7 @@ namespace WebApplication5.ashx
             {
                 sb .Append(s[r.Next(s.Length - 1)]);
             }
-            return sb;
+            return sb.ToString();
         }
 
         public bool IsReusable
