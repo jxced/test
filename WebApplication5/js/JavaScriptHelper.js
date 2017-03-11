96f003f517cbf9b472460c86204deddd6bde3db8
djx <jxce_d@163.com>
2017-03-11 12:03:12 +0800

    ok

---
+++ WebApplication5/js/JavaScriptHelper.js
@@ -0,0 +1,15 @@
+﻿var JsHelper = {
+
+};
+var jsonSerializerInAJAX = {
+    XmlHttpRequester: function () {
+        var xhrer;
+        if (window.XMLHttpRequest()) {
+            xhrer = new XMLHttpRequest();
+        } else {
+            xhrer =new ActiveXObject("Microsoft.XMLHTTp");
+        }
+        return xhrer;
+    }
+
+}
