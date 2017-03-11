d7b2c4a2b6d16503af393311211d1ef8f583a7be
djx <jxce_d@163.com>
2017-03-11 17:28:27 +0800

    ok

--- WebApplication5/js/JavaScriptHelper.js
+++ WebApplication5/js/JavaScriptHelper.js
@@ -10,6 +10,26 @@ var jsonSerializerInAJAX = {
             xhrer =new ActiveXObject("Microsoft.XMLHTTp");
         }
         return xhrer;
+    },
+    GetMethodByAJAX:function() {
+        
+    },
+    PostMethodByAJAX:function () {
+        
+    },
+    AJAXProcessMethod:function (method,url,params,callBackfunc) {
+        var xhrer=this.XmlHttpRequester();
+        var HttpMethod=method.toLowerCase();
+        xhrer.open(method,url,true);
+        if (HttpMethod=="get") {
+            xhrer.setRequestHeader("If-Modified-Since","0");
+        }
+        if (HttpMethod=="post") {
+            xhrer.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        }
+        xhrer.onreadystatechange=function () {
+                
+        }
     }
 
 }
